d5fbe48cb376d3bd3ade6e78e8a324bfe525b906
akhil76 <akhilpaulweb@gmail.com>
2022-06-20 21:47:57 +0600

    something.

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const router = require('./routers/router');
 
 const app = express();
-const PORT ="3001";
+dotenv.config();
+app.use(cors());
+app.use(express.json({limit:'50mb',extended:'true'}));
+app.use(express.urlencoded({limit:'50mb',extended:'true'}));
+app.use('/',router);
 
+
+const PORT = process.env.PORT;
+Db_url = `mongodb+srv://${process.env.User}:${process.env.Password}@bank.klnpa.mongodb.net/${process.env.DbName}?retryWrites=true&w=majority`;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
-
+    mongoose.connect(Db_url,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        //useFindAndModify: false // for this ,database is not connected.
+    })
+    .then(()=>{
+        console.log('Database is connected..');
+    })
+    .catch(()=>{
+        console.log('Database is not connected!'); 
+    })
 
 });
